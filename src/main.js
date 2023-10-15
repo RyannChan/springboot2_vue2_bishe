a12c74952879dd02b7bee2f927b89c7677155fec
Ryann <2072364932@qq.com>
2023-10-15 13:34:47 +0800

    SpringBoot+Vue后台管理系统

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/global.css'
+import request from './utils/request'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI,{size:"mini"})
+Vue.config.request = request
 
 new Vue({
   router,
